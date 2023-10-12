375e5f51e808668b42a5fc92b54aa8dc21c5a209
yuzu <like09th@gmail.com>
2023-10-12 17:50:30 +0800

    feat: modify account

--- v2/jsonObject/account.go
+++ v2/jsonObject/account.go
@@ -2,19 +2,19 @@ package jsonObject
 
 import v2 "github.com/YuzuWiki/Pixivlee/v2"
 
-type info struct {
+type userData struct {
 	Name         string `json:"name"`
 	PrivacyLevel string `json:"privacyLevel"`
 }
 
 type User struct {
-	UserID     v2.TPid `json:"userId,string"`
-	Name       string  `json:"name"`
-	Avatar     string  `json:"imageBig"`
-	IsFollowed bool    `json:"isFollowed"`
-	Following  int32   `json:"following"`
-	Region     info    `json:"region"`
-	Gender     info    `json:"gender"`
-	BirthDay   info    `json:"birthDay"`
-	Job        info    `json:"job"`
+	UserID     v2.TPid  `json:"userId,string"`
+	Name       string   `json:"name"`
+	Avatar     string   `json:"imageBig"`
+	IsFollowed bool     `json:"isFollowed"`
+	Following  int32    `json:"following"`
+	Region     userData `json:"region"`
+	Gender     userData `json:"gender"`
+	BirthDay   userData `json:"birthDay"`
+	Job        userData `json:"job"`
 }
